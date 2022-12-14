2c6681f49241ebfc6b2018f769611eda81701cfa
Thor Anker Kvisgård Lange <tal@netic.dk>
2022-12-14 09:14:54 +0100

    feat: Workflow reuse

---
+++ .github/scripts/helm-repos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add vector https://helm.vector.dev
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add cert-manager https://charts.jetstack.io
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
